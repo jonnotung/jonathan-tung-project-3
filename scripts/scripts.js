b06c169211e4373404e49d0156247c388561ea54
Jonathan Tung <jonno.tung@gmail.com>
2019-05-15 18:01:26 -0400

    added to wordLibrary

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,12 @@
 const wordLibrary = ["Finch", "North York Center", "Sheppard", "York Mills", "Lawrence", "Eglinton", "Davisville", "St Clair", "Summerhill", "Rosedale", "Bloor", "Wellesley", "College", "Dundas", "Queen", "King", "Union", "St Andrew", "Osgoode", "St Patrick", "Museum", "St George", "Spadina", "Dupont", "Glencairn", "Yorkdale", "Wilson", "Downsview", "Kipling", "Islington", "Royal York", "Old Mill", "Jane", "Runnymede", "High Park", "Keele", "Lansdowne", "Dufferin", "Ossington", "Christie", "Bathurst", "Bay", "Sherbourne", "Castle Frank", "Broadview", "Chester", "Pape", "Donlands", "Greenwood", "Coxwell", "Woodbine", "Main Street", "Victoria Park", "Warden", "Kennedy", "Ellesmere", "Midland", "STC", "Bayview", "Bessarion", "Leslie", "Don Mills", "McCowan",
+
 "Skydome", "Blue Jays", "Rogers center", "Joe Carter", "Roberto Alomar", "Roger Clemens", "Lloyd Moseby", "Jose Bautista", "Carlos Delgado", "Roy Halladay", "ACC", "Vince Carter", "Curtis Joseph" ,"DeMar DeRozan", "Kyle Lowry", "Tracy McGrady", "Kawhi Leonard", "Chris Boch", "Damon Stoudamire", "Maple Leaf Gardens", "Mats Sundin", "Dave Keon", "Darryl Sittler", "Doug Gilmour", "Johnny Bower", "Borje Salming", "Tim Horton", "Wendel  Clark",
-"CN tower", "Steamwhistle brewery", "Queens quay", "Ripley's aquarium",
-"Cabbagetown", "Corktown", "Leslieville", "The beaches", "Sunnyside", "Junction", "Annex", "Koreatown", "North York", "Etobicoke", "Scarborough", "East York", "Toronto", "Ward's Island", "Center Island", "Centerville", "Olympic Island", "Riverdale", "Kensington Market", "Chinatown", "Guildwood", "Harbourfront", "Queensway",
- "U of T", "Yonge", "Ryerson", "York", "Humber", "Centennial", "George Brown", "Seneca"];
+
+"CN tower", "Steamwhistle brewery", "Queens quay", "Ripley's aquarium", "Wonderland", "Ontario place", "CNE",
+
+"Cabbagetown", "Corktown", "Leslieville", "The beaches", "Sunnyside", "Junction", "Annex", "Koreatown", "North York", "Etobicoke", "Scarborough", "East York", "Toronto", "Ward's Island", "Center Island", "Centerville", "Olympic Island", "Riverdale", "Kensington Market", "Chinatown", "Guildwood", "Harbourfront", "Queensway", "Richview", "Ledbury Park", "Graydon Hall", "Riverside", "Woodbridge", "Christie pits", "Allan Gardens", "Casa Loma", "Baldwin village", "Bloorcourt", "Leaside", "Bloordale", "Brockton", "Church Wellsely Village", "Cityplace", "Little Italy", "Little Portugal", "Greektown", "Danforth", "Davenport", "Distillery District", "Dovercourt", "East Chinatown", "Forest Hill", "Harbord Village", "Keelesdale", "King East", "King West", "Liberty Village", "Little India", "Little Tokyo", "Markham", "Mississauga", "Moss Park", "Mount Pleasant", "Oakwood Village", "Port Lands", "Regent Park", "Richmond Hill", "Roncesvalles", "St James Town", "St Lawrence Market", "Stockyards", "Thorncliffe park", "Thornhill", "Vaughn", "Weston", "Woodbridge", "Yorkville", "Yonge",
+
+"U of T", "Ryerson", "York", "Humber", "Centennial", "George Brown", "Seneca"];
 
 //function to only capitalize first string in argument
 const makeCapitalized = function(ourString){
